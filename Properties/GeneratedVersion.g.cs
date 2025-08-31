e3d2e3d1713c667dc03493236067d7bbc9e01468
taipeiviking <clas.sivertsen@gmail.com>
2025-08-31 14:47:48 +0800

    installer: rebuild MSI (v1.78)

--- Properties/GeneratedVersion.g.cs
+++ Properties/GeneratedVersion.g.cs
@@ -1,6 +1,6 @@
 namespace WindowsDCRC {
     internal static partial class BuildInfo {
-        internal const string GeneratedVersion = "1.76";
-        internal const string GeneratedBuildDateUtc = "2025-08-31 06:29:42";
+        internal const string GeneratedVersion = "1.78";
+        internal const string GeneratedBuildDateUtc = "2025-08-31 06:47:00";
     }
 }
